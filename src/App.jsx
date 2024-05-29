1a231f782705a89dccb66c971fc960ad23013080
unknown <satyajitrakhunde@gmail.com>
2024-05-29 15:01:35 +0530

    user cant go to user route without login

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
-import  {lazy} from 'react'
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-const Home =lazy(()=>import("./pages/Home"))
-const Login =lazy(()=>import("./pages/Login"))
-const Chat =lazy(()=>import("./pages/Chat"))
-const Groups =lazy(()=>import("./pages/Groups"))
+import ProtectRoute from './components/auth/ProtectRoute';
+
+const Home = lazy(() => import('./pages/Home'));
+const Login = lazy(() => import('./pages/Login'));
+const Chat = lazy(() => import('./pages/Chat'));
+const Groups = lazy(() => import('./pages/Groups'));
 
 const App = () => {
-
-let user =true
-
+  const user = false; // Set this based on your authentication logic
   
   return (
-<Router>
-  <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/login" element={<Login/>} />
-    <Route path="/chat" element={<Chat/>} />
-    <Route path="/groups" element={<Groups/>} />
-    <Route path="/about" element={<h1>about</h1>} />
-    <Route path="*" element={<h1>Page Not Found</h1>} />
-  </Routes>
-</Router>   
-  )
-}
+    <Router>
+      {/* <Suspense fallback={<div>Loading...</div>}> */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route element={<ProtectRoute user={user}/>}>
+          <Route path="/" element={<Home />} />
+          <Route path="/chat" element={<Chat/>} />
+          <Route path="/groups" element={<Groups />} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Route>
+        </Routes>
+      {/* </Suspense> */}
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- src/components/auth/ProtectRoute.jsx
+++ src/components/auth/ProtectRoute.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+// import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-const ProtectRoute = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+//route to redirect to login when user is not logged in (preventing unlogged user to go home,chat)
+const ProtectRoute = ({ children, user, redirect = "/login" }) => {
+  if (!user) {
+    return <Navigate to={redirect} />;
+  }
+  return children ? children :<Outlet/>;
+};
 
-export default ProtectRoute
+export default ProtectRoute;
