500085351fd74b84fc6ab709616102741ab1321a
unknown <satyajitrakhunde@gmail.com>
2024-05-29 16:30:59 +0530

    once user login he cant go back to login, he will redirected to '/' path

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,12 @@ const Chat = lazy(() => import('./pages/Chat'));
 const Groups = lazy(() => import('./pages/Groups'));
 
 const App = () => {
-  const user = false; // Set this based on your authentication logic
+  const user = true; // Set this based on your authentication logic
   
   return (
     <Router>
       {/* <Suspense fallback={<div>Loading...</div>}> */}
         <Routes>
-          <Route path="/login" element={<Login />} />
           <Route element={<ProtectRoute user={user}/>}>
           <Route path="/" element={<Home />} />
           <Route path="/chat" element={<Chat/>} />
@@ -22,6 +21,7 @@ const App = () => {
           <Route path="/about" element={<h1>About</h1>} />
           <Route path="*" element={<h1>Page Not Found</h1>} />
           </Route>
+          <Route path="/login" element={<ProtectRoute user={!user} redirect='/'><Login /></ProtectRoute>} />
         </Routes>
       {/* </Suspense> */}
     </Router>
