13fd33e461d2682f2b7b98b99331fe9d9dc6385f
Satyajit <satyajitrakhunde@gmail.com>
2024-05-30 22:31:04 +0530

    chat list (left side )done

--- src/components/shared/AvatarCard.jsx
+++ src/components/shared/AvatarCard.jsx
@@ -1,9 +1,33 @@
-import { Stack } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Stack } from '@mui/material'
 import React from 'react'
 
+//to do transform
 const AvatarCard = ({avatar =[],max =4}) => {
   return (
-    <Stack direction={"row"} spacing={0.5}></Stack>
+    <Stack direction={"row"} spacing={0.5}>
+
+        <AvatarGroup max={max}> 
+        <Box width={"5rem"} height={"3rem"}>
+        {avatar.map((i ,index)=>(
+              <Avatar
+            key={Math.random()*100}
+            src={i}
+            alt={`Avatar ${index}`}
+            sx={{
+                width:"3rem",
+                height:"3rem",
+                border:"2px solid white",
+                position:"absolute",
+                left:{
+                    xs:`${0.5 +index}rem`,
+                    sm:`${index}rem`, 
+                }
+            }}
+            />
+        ))}
+        </Box>
+        </AvatarGroup>
+    </Stack>
   )
 }
 
