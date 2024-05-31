a88f27991be8692697e7f640bc9bc65fb0cd8979
Satyajit <satyajitrakhunde@gmail.com>
2024-05-31 09:41:00 +0530

    Notification dialog complete

--- src/components/specific/Notifications.jsx
+++ src/components/specific/Notifications.jsx
@@ -1,21 +1,62 @@
-// import React from 'react'
-
-import { DialogTitle, Stack, Typography,Dialog } from "@mui/material"
-import { SampleNotifications } from "../constants/SampleData"
+import { Avatar, Button, Dialog, DialogTitle, ListItem, Stack, Typography } from "@mui/material";
+import React, { memo } from 'react';
+import { SampleNotifications } from "../constants/SampleData";
 
 const Notifications = () => {
-  return (
-  <Dialog open>
-    <Stack p={{xs:"1rem",sm:"2rem"}} maxWidth={"25rem"}>
-      <DialogTitle>Notifications</DialogTitle>
-{
-  SampleNotifications.length > 0 ?<></>:<Typography textAlign={"center"}>0 Notificatios</Typography>
-}
-      
-    </Stack>
-  </Dialog>
-  
-  )
-}
+  const friendRequestHandler = (id, accept) => {
+    console.log(id, accept);
+  };
 
-export default Notifications
+  return (
+    <Dialog open>
+      <Stack p={{ xs: "1rem", sm: "2rem" }} maxWidth={"25rem"}>
+        <DialogTitle>Notifications</DialogTitle>
+        {
+          SampleNotifications.length > 0 ? (
+            SampleNotifications.map((i) =>
+              <NotificationsItem 
+                key={i._id} 
+                sender={i.sender} 
+                _id={i._id} 
+                handler={friendRequestHandler} 
+              />
+            )
+          ) : (
+            <Typography textAlign={"center"}>0 Notifications</Typography>
+          )
+        }
+      </Stack>
+    </Dialog>
+  );
+};
+
+const NotificationsItem = memo(({ sender, _id, handler }) => {
+  const { name, avatar } = sender;
+  return (
+    <ListItem>
+      <Stack direction={"row"} alignItems={'center'} spacing={"1rem"} width={"100%"}>
+        <Avatar src={avatar} />
+        <Typography
+          variant='body1'
+          sx={{
+            flexGrow: 1,
+            display: "-webkit-box",
+            WebkitLineClamp: 1,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            width: "100%"
+          }}
+        >
+          {`${name} sent you a friend request`}
+        </Typography>
+        <Stack direction={{ xs: "column", sm: "row" }}>
+          <Button onClick={() => handler({_id, accept:true})}>Accept</Button>
+          <Button color="error" onClick={() => handler({_id, accept:false})}>Reject</Button>
+        </Stack>
+      </Stack>
+    </ListItem>
+  );
+});
+
+export default Notifications;
