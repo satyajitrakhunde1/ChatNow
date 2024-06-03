40a1ba52be946112817f0de008a6e9bba9155c44
Satyajit <satyajitrakhunde@gmail.com>
2024-06-03 19:10:10 +0530

    frontend done

--- src/pages/admin/MessageManagement.jsx
+++ src/pages/admin/MessageManagement.jsx
@@ -1,12 +1,137 @@
-import React from 'react'
-import AdminLayout from '../../components/layout/AdminLayout'
+import { useFetchData } from "6pp";
+import { Avatar, Box, Skeleton, Stack } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import AdminLayout from "../../components/layout/AdminLayout";
+import AvatarCard from "../../components/shared/AvatarCard";
+import Table from "../../components/shared/Table";
+// import { server } from "../../constants/config";
+// import { useErrors } from "../../hooks/hook";
+import { dashboardData } from "../../components/constants/SampleData";
+import moment from "moment";
+import { fileFormat, transformImage } from "../../lib/feature";
+import RenderAttachment from "../../components/shared/RenderAttachment";
 
+const columns = [
+  {
+    field: "id",
+    headerName: "ID",
+    headerClassName: "table-header",
+    width: 200,
+  },
+  {
+    field: "attachments",
+    headerName: "Attachments",
+    headerClassName: "table-header",
+    width: 200,
+    renderCell: (params) => {
+      const { attachments } = params.row;
+
+      return attachments?.length > 0
+        ? attachments.map((i) => {
+            const url = i.url;
+            const file = fileFormat(url);
+
+            return (
+              <Box>
+                <a
+                  href={url}
+                  download
+                  target="_blank"
+                  style={{
+                    color: "black",
+                  }}
+                >
+                  {RenderAttachment(file, url)}
+                </a>
+              </Box>
+            );
+          })
+        : "No Attachments";
+    },
+  },
+
+  {
+    field: "content",
+    headerName: "Content",
+    headerClassName: "table-header",
+    width: 400,
+  },
+  {
+    field: "sender",
+    headerName: "Sent By",
+    headerClassName: "table-header",
+    width: 200,
+    renderCell: (params) => (
+      <Stack direction={"row"} spacing={"1rem"} alignItems={"center"}>
+        <Avatar alt={params.row.sender.name} src={params.row.sender.avatar} />
+        <span>{params.row.sender.name}</span>
+      </Stack>
+    ),
+  },
+  {
+    field: "chat",
+    headerName: "Chat",
+    headerClassName: "table-header",
+    width: 220,
+  },
+  {
+    field: "groupChat",
+    headerName: "Group Chat",
+    headerClassName: "table-header",
+    width: 100,
+  },
+  {
+    field: "createdAt",
+    headerName: "Time",
+    headerClassName: "table-header",
+    width: 250,
+  },
+];
 const MessageManagement = () => {
+  // const { loading, data, error } = useFetchData(
+  //   `${server}/api/v1/admin/chats`,
+  //   "dashboard-chats"
+  // );
+
+  // useErrors([
+  //   {
+  //     isError: error,
+  //     error: error,
+  //   },
+  // ]);
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    // if (data) {
+      setRows(
+        dashboardData.messages.map((i) => ({
+          ...i,
+          id: i._id,
+          sender: {
+            name: i.sender.name,
+            avatar: transformImage(i.sender.avatar, 50),
+          },
+          createdAt: moment(i.createdAt).format("MMMM Do YYYY, h:mm:ss a"),
+        }))
+      );
+    // }
+  }, []);
+
   return (
     <AdminLayout>
-      message management
+      {/* {loading ? (
+        <Skeleton height={"100vh"} />
+      ) : ( */}
+        <Table
+          heading={"All Messages"}
+          columns={columns}
+          rows={rows}
+          rowHeight={200}
+        />
+      {/* )} */}
     </AdminLayout>
-  )
-}
+  );
+};
 
-export default MessageManagement
+export default MessageManagement;
